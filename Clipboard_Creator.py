769bff3b0a8ad099db0b2e10a8a6a6e8d451b6a0
Its-Menace <ryan@jinnettes.com>
2023-11-16 11:37:36 -0600

    First commit

---
+++ Clipboard_Creator.py
@@ -0,0 +1,80 @@
+import pynput.keyboard
+import pyperclip as pc
+import datetime as dt
+from pynput.keyboard import Controller, Key
+import time
+
+kb = Controller()
+
+
+def clear_cb():
+    kb.press(Key.ctrl_l)
+    kb.press(Key.shift_l)
+    kb.press('x')
+    kb.release(Key.ctrl_l)
+    kb.release(Key.shift_l)
+    kb.release('x')
+    time.sleep(0.5)
+    kb.press(Key.enter)
+    kb.release(Key.enter)
+    time.sleep(0.5)
+
+
+def open_ray():
+    kb.press(Key.cmd)
+    kb.press(Key.shift_l)
+    kb.press('c')
+
+
+def release_ray():
+    kb.release(Key.cmd)
+    kb.release(Key.shift_l)
+    kb.release('c')
+
+
+def pin():
+    kb.press(Key.cmd)
+    kb.press(Key.shift_l)
+    kb.press('p')
+    time.sleep(1)
+    kb.release(Key.cmd)
+    kb.release(Key.shift_l)
+    kb.release('p')
+
+
+def clear_pin():
+    pin()
+
+
+def get_format_date():
+    global file_prefix
+    date = str(dt.datetime.now())
+    month = date[5:7]
+    day = date[8:10]
+    if month[0] == '0':
+        month = date[6:7]
+    file_prefix = 'Jinnette.' + month + '_' + day + '.'
+    print(file_prefix)
+    pc.copy(file_prefix)
+
+
+def main():
+    open_ray()
+    time.sleep(0.5)
+    release_ray()
+    time.sleep(0.5)
+    clear_cb()
+    clear_pin()  # clears any prior pin
+    clear_cb()
+    time.sleep(0.5)
+    # kb.tap(Key.esc)
+    get_format_date()  # grabs/formats date
+    time.sleep(2)
+    # open_ray() #launches ClipB manager
+    time.sleep(0.5)
+    # release_ray() #makes sure no keys are still pressed
+    pin()  # pins the latest entry to ClipB
+
+
+if __name__ == main():
+    main()
---
+++ cb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title cb
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description uses clipboard creator
+
+cd /Users/ryanjinnette/PycharmProjects/Clipboard
+python3 Clipboard_Creator.py
+
